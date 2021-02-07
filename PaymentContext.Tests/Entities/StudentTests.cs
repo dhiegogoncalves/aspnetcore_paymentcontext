6febcc608186cd0c663eafbef824853c96c520c2
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2021-02-07 17:02:10 -0400

    Implementação das Entities e ValueObjects;

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var student = new Student("Dhiego", "Silva", "2323047320", "dhiego@email.com");
+        }
+    }
+}
